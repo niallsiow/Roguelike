265197673aca537fb24d0a7432048e0e05b5f3c1
niallsiow@yahoo.co.uk <niallsiow@yahoo.co.uk>
2020-12-12 08:42:20 +0000

    made player rotate towards cursor position, bullet fires towards cursor

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -12,17 +12,31 @@ public class PlayerBehaviour : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        // rotate Player towards cursor position
+        /*
+        Vector3 cursorPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 myPos = new Vector2(transform.position.x, transform.position.y);
+        Vector2 diffPos = myPos - new Vector2(cursorPosition.x, cursorPosition.y);
+        float rotation = Mathf.Atan2(diffPos.x, diffPos.y);
+        transform.Rotate(0, 0, rotation);
+        
+        */
+        Vector3 cursorPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 diffPos = cursorPosition - transform.position;
+        diffPos.z = 0f;
+        transform.right = diffPos;
+
         // movement
         Vector2 movementVector;
         movementVector.x = Input.GetAxisRaw("Horizontal");
         movementVector.y = Input.GetAxisRaw("Vertical");
-        
-
         gameObject.GetComponent<Rigidbody2D>().velocity = movementVector.normalized * speed;
 
         // attack
@@ -31,11 +45,11 @@ public class PlayerBehaviour : MonoBehaviour
             Attack();
         }
 
-        
     }
 
     void Attack()
     {
-        Instantiate(bulletPrefab, transform.position, transform.rotation);
+        GameObject bullet = (GameObject)Instantiate(bulletPrefab, transform.position, transform.rotation);
+        bullet.transform.right = transform.right;
     }
 }
